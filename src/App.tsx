ecdba927d0ca1e7e66aa8c2eefcd86d92e481d0e
Jason Kobuchi <jekobuchi@gmail.com>
2022-11-30 14:34:52 -0800

    moved data import to Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import React, { useState, FC, useEffect } from 'react';
 import Navbar from './components/Navbar/Navbar';
-import data from './data/siteData.json'
 import './App.css';
-
-
 
 const App: FC = () => {
 
   return (
     <div className='App'>
-      <Navbar 
-        destinations={data.destinations} 
-        crew={data.crew} 
-        technology={data.technology}
-      />
+      <Navbar />
     </div>
   );
 }
