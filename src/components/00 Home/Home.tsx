f54f0a52c51c3c7e72a185fba5bb37a4b6c1ab45
Jason Kobuchi <jekobuchi@gmail.com>
2022-12-02 10:36:36 -0800

    added initial styles

--- src/components/00 Home/Home.tsx
+++ src/components/00 Home/Home.tsx
@@ -4,19 +4,16 @@ import "./Home.css";
 const Home = () => {
   return (
     <div className="home-container">
-      <div className="home-subcontainer">
-        <div className="home-blurb">
-          <h3>So, you want to travel to</h3>
-          <h1>SPACE</h1>
-          <p>Let’s face it; if you want to go to space, you might as well genuinely go to 
-          outer space and not hover kind of on the edge of it. Well sit back, and relax 
-          because we’ll give you a truly out of this world experience!</p>
-        </div>
-        <div className="home-explore">
-          <button>EXPLORE</button>
-        </div>
+      <div className="home-blurb">
+        <h3>So, you want to travel to</h3>
+        <h1>SPACE</h1>
+        <p>Let’s face it; if you want to go to space, you might as well genuinely go to 
+        outer space and not hover kind of on the edge of it. Well sit back, and relax 
+        because we’ll give you a truly out of this world experience!</p>
       </div>
-
+      <div className="home-explore">
+        <button>EXPLORE</button>
+      </div>
     </div>
   );
 };
