adaecbe2387162a3fedf49524d927f340d19c162
Jason Kobuchi <jekobuchi@gmail.com>
2022-12-07 11:28:41 -0800

    styled crew page to desktop standard before refinement

--- src/components/02 Crew/Crew.tsx
+++ src/components/02 Crew/Crew.tsx
@@ -8,24 +8,43 @@ interface CrewProps {
     role: string;
     bio: string;
   }[];
+  crewMember: number;
+  setCrewMember: (crewIdx: number) => void;
 }
 
-const Crew: FC<CrewProps> = ({ crew }) => {
+const Crew: FC<CrewProps> = ({ 
+  crew, 
+  crewMember, 
+  setCrewMember 
+}) => {
+
+  const changeCrewMember = (crewIdx: number): void => {
+    setCrewMember(crewIdx);
+  };
+
   return (
     <div className="crew-container">
-      <div className="crew-blurb">      
-        <p><span>02</span> MEET YOUR CREW</p>
-        <div className="crew-description">
-          <h1>{crew[0].role}</h1>
-          <p>{crew[0].name}</p>
-          <p>{crew[0].bio}</p>
-        </div>
-        <div className="crew-nav">          
-          <p>mini nav menu</p>
+      <div className="crew-blurb">
+        <div className="crew-blurb-sub">
+          <p className="crew-heading heading-5"><span className="grey">02</span> MEET YOUR CREW</p>
+          <div className="crew-description">
+            <p className="crew-role heading-4 grey">{crew[`${crewMember}`].role.toUpperCase()}</p>
+            <p className="crew-name heading-3">{crew[`${crewMember}`].name.toUpperCase()}</p>
+            <p className="crew-bio body-text lavender">{crew[`${crewMember}`].bio}</p>
+          </div>
+          <div className="crew-nav subheading-2">
+                <button onClick={() => changeCrewMember(0)} className={crewMember===0?"crewActiveOn":"crewActiveOff"}></button>
+                <button onClick={() => changeCrewMember(1)} className={crewMember===1?"crewActiveOn":"crewActiveOff"}></button>
+                <button onClick={() => changeCrewMember(2)} className={crewMember===2?"crewActiveOn":"crewActiveOff"}></button>
+                <button onClick={() => changeCrewMember(3)} className={crewMember===3?"crewActiveOn":"crewActiveOff"}></button>
+          </div>
         </div>
       </div>
       <div className="crew-img">
-        <img />
+        <img 
+          src={require(`../../${crew[`${crewMember}`].images.png}.png`)}
+          alt={crew[`${crewMember}`].name}
+        />
       </div>
     </div>
   );
