bd5a9ea4a51aedd8d2aee87cee24dc6dfa1ca1d5
Jason Kobuchi <jekobuchi@gmail.com>
2022-12-13 09:07:17 -0800

    adjusted styles, added containers to accomodate tablet responsiveness

--- src/components/03 Technology/Technology.tsx
+++ src/components/03 Technology/Technology.tsx
@@ -23,32 +23,30 @@ const Technology: FC<TechnologyProps> = ({
 
   return (
     <div className="technology-container">
-
-      <div className="technology-blurb">
-        <div className="technology-blurb-sub">    
-          <p className="technology-heading heading-5"><span className="grey">03</span> SPACE LAUNCH 101</p>
-          <div className="technology-flex">
-            <div className="technology-nav subheading-2">          
-              <button onClick={() => changeTechnology(0)} className={tech===0?"techActiveOn":"techActiveOff"}>1</button>
-              <button onClick={() => changeTechnology(1)} className={tech===1?"techActiveOn":"techActiveOff"}>2</button>
-              <button onClick={() => changeTechnology(2)} className={tech===2?"techActiveOn":"techActiveOff"}>3</button>
+      <p className="technology-heading heading-5"><span className="grey">03</span> SPACE LAUNCH 101</p>
+      <div className="technology-container-sub">
+        <div className="technology-blurb">    
+            <div className="technology-flex">
+              <div className="technology-nav subheading-2">          
+                <button onClick={() => changeTechnology(0)} className={tech===0?"techActiveOn":"techActiveOff"}>1</button>
+                <button onClick={() => changeTechnology(1)} className={tech===1?"techActiveOn":"techActiveOff"}>2</button>
+                <button onClick={() => changeTechnology(2)} className={tech===2?"techActiveOn":"techActiveOff"}>3</button>
+              </div>
+              <div className="technology-description">
+                <p className="technology-role subheading-1 lavender">THE TERMINOLOGY...</p>
+                <p className="technology-name heading-3">{technology[`${tech}`].name.toUpperCase()}</p>
+                <p className="technology-desc body-text lavender">{technology[`${tech}`].description}</p>
+              </div>            
             </div>
-            <div className="technology-description">
-              <p className="tech-role subheading-1 lavender">THE TERMINOLOGY...</p>
-              <p className="tech-name heading-3">{technology[`${tech}`].name.toUpperCase()}</p>
-              <p className="tech-desc body-text lavender">{technology[`${tech}`].description}</p>
-            </div>            
+          </div>
+
+          <div className="technology-img">
+            <img 
+              src={require(`../../${technology[`${tech}`].images.portrait}.jpg`)}
+              alt={technology[`${tech}`].name}
+            />
           </div>
         </div>
-      </div>
-
-      <div className="technology-img">
-        <img 
-          src={require(`../../${technology[`${tech}`].images.portrait}.jpg`)}
-          alt={technology[`${tech}`].name}
-        />
-      </div>
-
     </div>
   );
 };
