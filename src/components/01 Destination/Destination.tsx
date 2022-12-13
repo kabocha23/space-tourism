cd35633480ab8aa8842878f302c27450e6d84de7
Jason Kobuchi <jekobuchi@gmail.com>
2022-12-13 09:06:48 -0800

    adjusted styles, added containers to accomodate tablet responsiveness

--- src/components/01 Destination/Destination.tsx
+++ src/components/01 Destination/Destination.tsx
@@ -26,38 +26,39 @@ const Destination: FC<DestinationProps> = ({
 
   return (
     <div className="destination-container">
+      <p className="destination-heading heading-5"><span className="grey">01</span> PICK YOUR DESTINATION</p>
+      <div className="destination-container-sub">
+        <div className="destination-img">      
+          <img 
+            src={require(`../../${destinations[`${planet}`].images.png}.png`)}
+            alt={destinations[`${planet}`].name}
+            />
+        </div>
 
-      <div className="destination-img">      
-        <p className="heading-5"><span className="grey">01</span> PICK YOUR DESTINATION</p>
-        <img 
-          src={require(`../../${destinations[`${planet}`].images.png}.png`)}
-          alt={destinations[`${planet}`].name}
-          />
-      </div>
-
-      <div className="destination-blurb">
-        <div className="destination-blurb-sub">
-          <div className="destination-blurb-top">
-            <div className="destination-nav subheading-2">
-              <p onClick={() => changePlanet(0)} className={planet===0?"planetActiveOn":"planetActiveOff"}>MOON</p>
-              <p onClick={() => changePlanet(1)} className={planet===1?"planetActiveOn":"planetActiveOff"}>MARS</p>
-              <p onClick={() => changePlanet(2)} className={planet===2?"planetActiveOn":"planetActiveOff"}>EUROPA</p>
-              <p onClick={() => changePlanet(3)} className={planet===3?"planetActiveOn":"planetActiveOff"}>TITAN</p>
+        <div className="destination-blurb">
+          <div className="destination-blurb-sub">
+            <div className="destination-blurb-top">
+              <div className="destination-nav subheading-2">
+                <p onClick={() => changePlanet(0)} className={planet===0?"planetActiveOn":"planetActiveOff"}>MOON</p>
+                <p onClick={() => changePlanet(1)} className={planet===1?"planetActiveOn":"planetActiveOff"}>MARS</p>
+                <p onClick={() => changePlanet(2)} className={planet===2?"planetActiveOn":"planetActiveOff"}>EUROPA</p>
+                <p onClick={() => changePlanet(3)} className={planet===3?"planetActiveOn":"planetActiveOff"}>TITAN</p>
+              </div>
+              <p className="destination-name heading-2">{destinations[`${planet}`].name.toUpperCase()}</p>
+              <p className="body-text lavender">{destinations[`${planet}`].description}</p>          
             </div>
-            <p className="destination-name heading-2">{destinations[`${planet}`].name.toUpperCase()}</p>
-            <p className="body-text lavender">{destinations[`${planet}`].description}</p>          
-          </div>
 
-          <hr></hr>
+            <hr></hr>
 
-          <div className="destination-blurb-bottom">
-            <div className="avg-distance">
-              <p className="subheading-2 lavender">AVG. DISTANCE</p>
-              <p className="heading-4 white">{destinations[`${planet}`].distance.toUpperCase()}</p>
-            </div>
-            <div className="est-travel-time">
-              <p className="subheading-2 lavender">EST. TRAVEL TIME</p>
-              <p className="heading-4 white">{destinations[`${planet}`].travel.toUpperCase()}</p>
+            <div className="destination-blurb-bottom">
+              <div className="avg-distance">
+                <p className="subheading-2 lavender">AVG. DISTANCE</p>
+                <p className="heading-4 white">{destinations[`${planet}`].distance.toUpperCase()}</p>
+              </div>
+              <div className="est-travel-time">
+                <p className="subheading-2 lavender">EST. TRAVEL TIME</p>
+                <p className="heading-4 white">{destinations[`${planet}`].travel.toUpperCase()}</p>
+              </div>
             </div>
           </div>
         </div>
