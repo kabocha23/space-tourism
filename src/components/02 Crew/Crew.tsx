99f6281246f41d5b3b74d6d7e44b9182b0f88272
Jason Kobuchi <jekobuchi@gmail.com>
2022-12-15 15:05:23 -0800

    adjusted containers to match other pages, aligning headers, text, and images between routes

--- src/components/02 Crew/Crew.tsx
+++ src/components/02 Crew/Crew.tsx
@@ -24,27 +24,29 @@ const Crew: FC<CrewProps> = ({
 
   return (
     <div className="crew-container">
-      <div className="crew-blurb">
-        <div className="crew-blurb-sub">
-          <p className="crew-heading heading-5"><span className="grey">02</span> MEET YOUR CREW</p>
-          <div className="crew-description">
-            <p className="crew-role heading-4 grey">{crew[`${crewMember}`].role.toUpperCase()}</p>
-            <p className="crew-name heading-3">{crew[`${crewMember}`].name.toUpperCase()}</p>
-            <p className="crew-bio body-text lavender">{crew[`${crewMember}`].bio}</p>
-          </div>
-          <div className="crew-nav subheading-2">
-                <button onClick={() => changeCrewMember(0)} className={crewMember===0?"crewActiveOn":"crewActiveOff"}></button>
-                <button onClick={() => changeCrewMember(1)} className={crewMember===1?"crewActiveOn":"crewActiveOff"}></button>
-                <button onClick={() => changeCrewMember(2)} className={crewMember===2?"crewActiveOn":"crewActiveOff"}></button>
-                <button onClick={() => changeCrewMember(3)} className={crewMember===3?"crewActiveOn":"crewActiveOff"}></button>
+      <p className="crew-heading heading-5"><span className="grey">02</span> MEET YOUR CREW</p>
+      <div className="crew-container-sub">
+        <div className="crew-blurb">
+          <div className="crew-blurb-sub">
+            <div className="crew-description">
+              <p className="crew-role heading-4 grey">{crew[`${crewMember}`].role.toUpperCase()}</p>
+              <p className="crew-name heading-3">{crew[`${crewMember}`].name.toUpperCase()}</p>
+              <p className="crew-bio body-text lavender">{crew[`${crewMember}`].bio}</p>
+            </div>
+            <div className="crew-nav subheading-2">
+                  <button onClick={() => changeCrewMember(0)} className={crewMember===0?"crewActiveOn":"crewActiveOff"}></button>
+                  <button onClick={() => changeCrewMember(1)} className={crewMember===1?"crewActiveOn":"crewActiveOff"}></button>
+                  <button onClick={() => changeCrewMember(2)} className={crewMember===2?"crewActiveOn":"crewActiveOff"}></button>
+                  <button onClick={() => changeCrewMember(3)} className={crewMember===3?"crewActiveOn":"crewActiveOff"}></button>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="crew-img">
-        <img 
-          src={require(`../../${crew[`${crewMember}`].images.png}.png`)}
-          alt={crew[`${crewMember}`].name}
-        />
+        <div className="crew-img">
+          <img 
+            src={require(`../../${crew[`${crewMember}`].images.png}.png`)}
+            alt={crew[`${crewMember}`].name}
+          />
+        </div>
       </div>
     </div>
   );
